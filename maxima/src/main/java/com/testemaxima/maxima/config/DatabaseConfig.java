0d48d30424bb7b3992951bde267a46bef5b78d6f
sara viana <saraviana04@gmail.com  git config --global user.name sara>
2024-12-30 21:37:04 -0300

    Configuração do banco de dados em memoria

--- maxima/src/main/java/com/testemaxima/maxima/config/DatabaseConfig.java
+++ maxima/src/main/java/com/testemaxima/maxima/config/DatabaseConfig.java
@@ -1,20 +1,11 @@
 package com.testemaxima.maxima.config;
 
-import org.h2.tools.Server;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-
-import java.sql.SQLException;
-
 
 @Configuration
 public class DatabaseConfig {
 
-    //aqui no bean abaixo e para inicializar o servidor com H2 automaticamente
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server h2Server() throws SQLException {
-        // Cria o servidor TCP com banco H2
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
-    }
 }
+
 
